d8ddea53db32aad8ac5504ad2ea5d02435ab78a5
pravar1919 <pcprakash1919@gmail.com>
2024-06-01 23:16:15 +0530

    docker command

--- core/settings.py
+++ core/settings.py
@@ -137,4 +137,5 @@ CHANNEL_LAYERS = {
         },
     },
 }
-# docker run -d --hostname my-rabbit -p 5672:15672 rabbitmq:3-management
+# docker run -d --name my-rabbit -p 5672:5672 rabbitmq:3-management
+# docker run -d --name my-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
